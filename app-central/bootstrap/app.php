ee09d3f0216b91080b2e6b6e2f981080dd1faca8
pequeguillermo <pequeguillermo@gmail.com>
2026-05-12 08:38:20 +0200

    arreglo login

--- app-central/bootstrap/app.php
+++ app-central/bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
